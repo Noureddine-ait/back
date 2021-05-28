312337aa35c063bfdbcef1f3d7bb8ea96878698a
Noureddine-ait <nour-eddine.aitlahcen@edu.uca.ma>
2021-05-28 13:03:53 +0100

    commite7

--- src/main/java/com/wilaya/courrierbackwillyia/dao/EntiteAdminDao.java
+++ src/main/java/com/wilaya/courrierbackwillyia/dao/EntiteAdminDao.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EntiteAdminDao  extends JpaRepository<EntiteAdmin, Long> {
 
-
     EntiteAdmin findByCode(String code);
     public int deleteByCode(String code);
 }
